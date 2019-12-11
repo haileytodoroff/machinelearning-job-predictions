ac28def4206a58c7eb4aec4970b3056a751e8bd0
Hailey Todoroff <ht2450@columbia.edu>
2019-12-11 14:05:03 -0500

    working cnn model using keras and tensorflow. good accuracy

--- kaggle_hw7.py
+++ kaggle_hw7.py
@@ -8,6 +8,10 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import svm, metrics
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+
 
 
 class Kaggle:
@@ -31,107 +35,116 @@ class Kaggle:
 		self.scan_test_data = np.load('scan-test-images.npy')
 		# print('scan test', self.scan_test_data[1])
 
-		# # flatten images
-		# n_samples = len(self.mnist_train_data)
-		# self.data = self.mnist_train_data.reshape((n_samples,-1))
 
-	# def svm(self):
-	# 	classifier = svm.SVC(gamma=0.001)
-
-	# 	#split data
-	# 	X_train, X_test, y_train, y_test = train_test_split(self.data, self.mnist_train_labels, test_size=0.4, shuffle=False)
-	# 	classifier.fit(X_train, y_train)
-	# 	print('train score:', classifier.score(X_train, y_train))
-	# 	print('test score', classifier.score(X_test, y_test))
-	# 	preds = classifier.predict(X_test)		
-
-	# 	print("Classification report for classifier %s:\n%s\n" % (classifier, metrics.classification_report(y_test, preds)))
-
-
-	# train/test/split, normalize, standardize, feature importance, etc
-	def preprocess(self):
+	def cnn(self):
 		# join data
 		data = np.concatenate((self.mnist_train_data, self.scan_train_images))
-		data_new = data.reshape((len(data), -1))
-		pred_data = self.scan_test_data.reshape((len(self.scan_test_data), -1))
-		# print(data_new.shape)
 		labels = np.concatenate((self.mnist_train_labels, self.scan_train_labels))
-		# print(labels.shape)
 
 		# train test split
-		X_train, X_test, y_train, y_test = train_test_split(data_new, labels, test_size=0.4)
+		X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2)
 
-		# # train test split on mnist and scan data
-		# mX_train, mX_test, my_train, my_test = train_test_split(self.mnist_train_data, self.mnist_train_labels)
-		# sX_train, sX_test, sy_train, sy_test = train_test_split(self.scan_train_images, self.scan_train_labels)
+		# add axis to account for grayscale axis (keras feature)
+		X_train = X_train[:,:,:, np.newaxis]
+		X_test = X_test[:,:,:, np.newaxis]
+		X_preds = self.scan_test_data[:,:,:,np.newaxis]
 
-		# # config training
-		# self.X_train = np.concatenate((mX_train, sX_train), axis=0)
-		# self.Y_train = np.concatenate((my_train, sy_train), axis=0)
+		# convert to float 
+		X_train = X_train.astype(np.float)
+		X_train /= 255
+		X_test = X_test.astype(np.float)
+		X_test /= 255
+		X_preds = X_preds.astype(np.float)
+		X_preds /= 255
 
-		# # config testing
-		# self.X_test = np.concatenate((mX_test, sX_test), axis=0)
-		# self.Y_test = np.concatenate((my_test, sy_test), axis=0)
+		# one hot encode
+		y_train = keras.utils.to_categorical(y_train, 10)
+		y_test = keras.utils.to_categorical(y_test, 10)
 
-		# # reshape the train data so that is can be used in sklearn (2d data)
-		# size = self.X_train.shape[0]
-		# self.resized_X_train = np.reshape(self.X_train, (size, 28*28))
+		classifier = Sequential([Conv2D(8,(3,3), activation='relu', input_shape=(28,28,1)),
+			MaxPooling2D(pool_size=(2,2)), Flatten(), Dense(10, activation='softmax')])
 
-		# # reshape the test data
-		# dims = self.X_test.shape
-		# self.resized_X_test = np.reshape(self.X_test, (dims[0], dims[1]*dims[2]))
+		classifier.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.SGD(),
+			metrics=['accuracy'])
 
-		# # reshape prediction data
-		# dims2 = self.scan_test_data.shape
-		# self.resized_X_preds = np.reshape(self.scan_test_data, (dims2[0], dims2[1]*dims2[2]))
-		return X_train, X_test, y_train, y_test, pred_data
+		# train model
+		classifier.fit(X_train, y_train, batch_size=16, verbose=1, validation_data=(X_test, y_test), epochs=7)
 
+		# predict 
+		preds = classifier.predict_classes(X_preds)
+		print('predictions', preds)
 
-	def randForrest(self, X_train, X_test, y_train, y_test, pred_data):
-		rf = RandomForestClassifier(n_estimators=100)
-		
-		rf.fit(X_train, y_train)
-		print('train score', rf.score(X_train, y_train))
-
-		print('test score', rf.score(X_test, y_test))
-
-		preds = rf.predict(pred_data)
-		print('predictions', preds[0:5])
-
-		# train_score = rf.score(self.resized_X_train, self.Y_train)
-		# test_score = rf.score(self.resized_X_test, self.Y_test)
-
-		# predict
-		# preds = rf.predict(self.resized_X_preds)
-		
 		return preds
 
 
 
 if __name__ == '__main__':
 	kaggle = Kaggle()
-
-	X_train, X_test, y_train, y_test, pred_data = kaggle.preprocess()
-
-	preds = kaggle.randForrest(X_train, X_test, y_train, y_test, pred_data)
-
-	# train_score, test_score, preds = kaggle.randForrest()
-	# print('train score', train_score)
-	# print('test score', test_score)
-
-	# print('preds array', preds[0:5])
-	predictions = pd.DataFrame(preds)
-	# # str_predictions = str(predictions)
-	# # for i in str_predictions:
-	# # 	i = i[0]
-	# # print('new predictions df', str_predictions)
-	# # print('preds df', predictions.head())
-	# # print('preds df shape', predictions.shape)
-	# # print('preds array len', len(preds))
-
-	# # save preds to csv
-	# with open('submission.csv', 'wb') as f:
-	# 	np.savetxt(f, preds, fmt='%.18e')
+	preds = kaggle.cnn()
+	predictions = pd.DataFrame(preds, columns=['Category'])
 	predictions.to_csv('submission.csv', index=True, header=['Category'])
+	
+
+
+##### failed other models/techniques (just ignore this!) :(
+# train/test/split, normalize, standardize, feature importance, etc
+def preprocess(self):
+	# join data
+	data = np.concatenate((self.mnist_train_data, self.scan_train_images))
+	data_new = data.reshape((len(data), -1))
+	pred_data = self.scan_test_data.reshape((len(self.scan_test_data), -1))
+	# print(data_new.shape)
+	labels = np.concatenate((self.mnist_train_labels, self.scan_train_labels))
+	# print(labels.shape)
+
+	# train test split
+	X_train, X_test, y_train, y_test = train_test_split(data_new, labels, test_size=0.25)
+
+
+	id_col = list(range(0,len(self.scan_test_data)))
+
+	# # train test split on mnist and scan data
+	# mX_train, mX_test, my_train, my_test = train_test_split(self.mnist_train_data, self.mnist_train_labels)
+	# sX_train, sX_test, sy_train, sy_test = train_test_split(self.scan_train_images, self.scan_train_labels)
+
+	# # config training
+	# self.X_train = np.concatenate((mX_train, sX_train), axis=0)
+	# self.Y_train = np.concatenate((my_train, sy_train), axis=0)
+
+	# # config testing
+	# self.X_test = np.concatenate((mX_test, sX_test), axis=0)
+	# self.Y_test = np.concatenate((my_test, sy_test), axis=0)
+
+	# # reshape the train data so that is can be used in sklearn (2d data)
+	# size = self.X_train.shape[0]
+	# self.resized_X_train = np.reshape(self.X_train, (size, 28*28))
+
+	# # reshape the test data
+	# dims = self.X_test.shape
+	# self.resized_X_test = np.reshape(self.X_test, (dims[0], dims[1]*dims[2]))
+
+	# # reshape prediction data
+	# dims2 = self.scan_test_data.shape
+	# self.resized_X_preds = np.reshape(self.scan_test_data, (dims2[0], dims2[1]*dims2[2]))
+	return X_train, X_test, y_train, y_test, pred_data, id_col
+
+def randForrest(self, X_train, X_test, y_train, y_test, pred_data):
+	rf = RandomForestClassifier(n_estimators=100)
+	
+	rf.fit(X_train, y_train)
+	print('train score', rf.score(X_train, y_train))
+
+	print('test score', rf.score(X_test, y_test))
+
+	preds = rf.predict(pred_data)
+	print('predictions', preds[0:5])
+
+	# train_score = rf.score(self.resized_X_train, self.Y_train)
+	# test_score = rf.score(self.resized_X_test, self.Y_test)
+
+	# predict
+	# preds = rf.predict(self.resized_X_preds)
+	
+	return preds
 
 
